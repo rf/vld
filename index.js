827f733a55c0951c371e78e16905bd4519b75600
Russ Frank <rf@uber.com>
2015-03-19 11:56:02 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,164 @@
+var TypedError = require('error/typed');
+
+var InvalidProperty = TypedError({
+    type: 'invalid.property',
+    message: "Expected property '{itemName}' of `{object}` to be " +
+        "{expectedStr}, but instead got {value} ({valueType})"
+});
+
+var ValidationError = TypedError({
+    type: 'validation',
+    message: "Expected {itemName} to be {expectedStr}, but instead got " +
+        "{value} ({valueType})"
+});
+
+function validateSingle(item, key, name, checkFn, errorConstructor) {
+    var res = checkFn(item, key);
+    if (res) {
+        res.object = name;
+        throw new errorConstructor(res);
+    }
+}
+
+var vld = {};
+
+function typeOfCheck(type) {
+    typeOfCheckInline.expected = type;
+
+    return typeOfCheckInline;
+
+    function typeOfCheckInline(item, name, errorConstructor) {
+        var ret;
+        if (errorConstructor !== false) {
+            errorConstructor = errorConstructor || ValidationError;
+        }
+
+        if (typeof item !== type && typeof item !== 'undefined') {
+            ret = {
+                value: item,
+                valueType: typeof item,
+                expected: type,
+                expectedStr: type,
+                itemName: name
+            };
+        }
+
+        if (ret && errorConstructor) {
+            throw new errorConstructor(ret);
+        } else {
+            return ret;
+        }
+    }
+
+    typeOfCheckInline.internalInterface = function typeofInternal(options) {
+        var res = typeOfCheckInline(options.value);
+    };
+}
+
+vld.string = typeOfCheck('string');
+vld.number = typeOfCheck('number');
+vld.function = typeOfCheck('function');
+vld.undefined = typeOfCheck('undefined');
+vld.object = typeOfCheck('object');
+
+vld.properties = function vldObject(obj, name, spec, errorConstructor) {
+    errorConstructor = errorConstructor || InvalidProperty;
+    Object.keys(spec).forEach(function (key) {
+        console.log("checking item " + key);
+        var item = obj[key];
+        var rules = spec[key];
+        if (Array.isArray(rules)) {
+            rules.forEach(function (rule) {
+                validateSingle(item, key, name, rule, errorConstructor);
+            });
+        } else {
+            validateSingle(item, key, name, rules, errorConstructor);
+        }
+    });
+};
+
+vld.required = function validateRequired(checkFn) {
+    validateRequiredInline.expected = checkFn.expected + ' (required)';
+
+    return validateRequiredInline;
+
+    function validateRequiredInline(item, name, errorConstructor) {
+        if (typeof item === "undefined") {
+            return {
+                value: item,
+                valueType: typeof item,
+                itemName: name,
+                required: true,
+                expected: checkFn.expected,
+                expectedStr: validateRequiredInline.expected
+            };
+        }
+
+        return checkFn(item, name, errorConstructor);
+    }
+};
+
+vld.or = function validateOr() {
+    var checkFns = Array.prototype.slice.call(arguments);
+    validateOrInline.expected = checkFns
+        .map(function (item) { return item.expected; })
+        .join(' or ');
+
+    return validateOrInline;
+
+    function validateOrInline(item, name, errorConstructor) {
+        var res = true;
+        checkFns.forEach(function (checkFn) {
+            // at least one checkFn passes
+            console.log("or running check " + checkFn.expected);
+            if (!checkFn(item, name, false)) {
+                res = false;
+            }
+        });
+
+        if (errorConstructor !== false) {
+            errorConstructor = errorConstructor || ValidationError;
+        }
+
+        if (res) {
+            var resInfo = {
+                value: item,
+                valueType: typeof item,
+                itemName: name,
+                expectedStr: validateOrInline.expected,
+                expected: validateOrInline.expected
+            };
+
+            if (errorConstructor) {
+                throw new errorConstructor(resInfo);
+            }
+
+            return resInfo;
+        }
+    }
+};
+
+vld.equals = function validateEquals(expectedValue) {
+    validateEqualsInline.expected = expectedValue;
+
+    return validateEqualsInline;
+
+    function validateEqualsInline(item, name, errorConstructor) {
+        if (errorConstructor !== false) {
+            errorConstructor = errorConstructor || ValidationError;
+        }
+
+        if (item !== expectedValue) {
+            return {
+                value: item,
+                valueType: typeof item,
+                itemName: name,
+                required: true,
+                expected: expectedValue,
+                expectedStr: expectedValue
+            };
+        }
+    }
+};
+
+module.exports = vld;
---
+++ test.js
@@ -0,0 +1,78 @@
+var vld = require('./index');
+var test = require('tape');
+
+test('basic properties validation', function (assert) {
+    var options = {
+        type: 'thing',
+        message: 'this is a string',
+        callback: function () { }
+    };
+
+    vld.object(options);
+    vld.properties(options, 'options', {
+      type: vld.required(vld.string),
+      message: vld.string,
+      callback: vld.required(vld.function)
+    });
+
+    assert.end();
+});
+
+test('standalone or', function (assert) {
+    var something = 'string';
+    vld.or(vld.string, vld.number)(something, 'argument 0 (something)');
+    assert.end();
+});
+
+function testIt(type, name, callback) {
+    vld.required(vld.string)(type, 'argument 0 (type)');
+    vld.required(vld.or(vld.function, vld.string))(name, 'argument 1 (name or callback)');
+    vld.function(callback, 'argument 2 (callback');
+}
+
+test('arguments validation, happy', function (assert) {
+    testIt('string lol', function () {});
+    assert.end();
+});
+
+test('arguments validation, happy2', function (assert) {
+    testIt('sting', 'also string', function () {});
+    assert.end();
+});
+
+test('arguments validation, or throws', function (assert) {
+    assert.throws(function () {
+        testIt('string', true);
+    }, /Expected argument 1 \(name or callback\) to be function or string, but instead got true \(boolean\)/);
+    assert.end();
+});
+
+test('arguments validation, string throws', function (assert) {
+    assert.throws(function () {
+        testIt(23084);
+    }, /Expected argument 0 \(type\) to be string, but instead got 23084 \(number\)/);
+    assert.end();
+});
+
+test('properties with required or', function (assert) {
+    vld.properties({foo: 38}, 'options', {
+        foo: vld.required(vld.or(vld.function, vld.string, vld.equals(38)))
+    });
+
+    vld.properties({foo: 'hi'}, 'options', {
+        foo: vld.required(vld.or(vld.function, vld.string, vld.equals(38)))
+    });
+
+    vld.properties({foo: function () {}}, 'options', {
+        foo: vld.required(vld.or(vld.function, vld.string, vld.equals(38)))
+    });
+
+    assert.throws(function () {
+        vld.properties({foo: 93}, 'options', {
+            foo: vld.required(vld.or(vld.function, vld.string, vld.equals(38)))
+        });
+    }, /Expected foo to be function or string or 38, but instead got 93 \(number\)/);
+
+    assert.end();
+});
+
